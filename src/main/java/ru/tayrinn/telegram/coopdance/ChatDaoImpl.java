eb45fc57fdd04c151d3b1a7f632d1ee1e873d011
tayrinn <tayrinn@yandex-team.ru>
2021-03-22 00:08:23 +0300

    sql

--- src/main/java/ru/tayrinn/telegram/coopdance/ChatDaoImpl.java
+++ src/main/java/ru/tayrinn/telegram/coopdance/ChatDaoImpl.java
@@ -46,7 +46,8 @@ public class ChatDaoImpl implements ChatDao {
                 ChatMessage.KEY_MESSAGE_ID + "," +
                 ChatMessage.KEY_AUTHOR_USERNAME + "," +
                 ChatMessage.KEY_IS_BOT + "," +
-                ChatMessage.KEY_PAYLOAD + "" +
+                ChatMessage.KEY_PAYLOAD + "," +
+                ChatMessage.KEY_TIMESTAMP + "" +
                 ")" +
                 " VALUES(" +
                 "'" + chatMessage.getChatId() + "', " +
