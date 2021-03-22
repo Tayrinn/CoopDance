1a1f742c26b5d580c7f71d494db7e47e65c778b1
tayrinn <tayrinn@yandex-team.ru>
2021-03-23 00:50:40 +0300

    /start parse %20

--- src/main/java/ru/tayrinn/telegram/coopdance/models/CallbackData.java
+++ src/main/java/ru/tayrinn/telegram/coopdance/models/CallbackData.java
@@ -1,8 +1,17 @@
 package ru.tayrinn.telegram.coopdance.models;
 
 public class CallbackData {
+    /**
+     * command
+     */
     public String c;
+    /**
+     * message
+     */
     public String m;
+    /**
+     * messageId
+     */
     public String i;
     public CallbackData() { /* empty for gson */ }
 
